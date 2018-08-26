6b566c5a0686c3f09b7a9bd0c47f1fb7ad4c810c
xincun <2212099931@qq.com>
2018-08-26 22:57:35 +0800

    test

---
+++ symbol-store/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button} from 'antd/lib';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+    );
+  }
+}
+
+export default App;
